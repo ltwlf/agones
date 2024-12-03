cfede6b353fcd1b88d604f8254b27be694636534
Christian Glessner <chris@leitwolf.io>
2024-12-03 18:46:25 +0100

    Update aks.tf

--- install/terraform/modules/aks/aks.tf
+++ install/terraform/modules/aks/aks.tf
@@ -15,10 +15,11 @@
 
 terraform {
   required_version = ">= 1.0.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.94.0"
+      version = ">= 3.94.0"  # Ensure this version or later is used
     }
   }
 }
@@ -27,56 +28,32 @@ provider "azurerm" {
   features {}
 }
 
-variable "client_id" {}
-variable "client_secret" {}
-variable "cluster_name" {
-  default = "test-cluster"
-}
-variable "resource_group_location" {
-  default = "northeurope"
-}
-variable "resource_group_name" {
-  default = "agonesRG"
-}
-variable "kubernetes_version" {
-  default = "1.27.1" # Replace with a supported Kubernetes version
-}
-variable "machine_type" {
-  default = "Standard_D2_v2"
-}
-variable "node_count" {
-  default = 4
-}
-variable "disk_size" {
-  default = 30
-}
-variable "enable_node_public_ip" {
-  default = false
-}
-
-# Resource group for AKS
 resource "azurerm_resource_group" "agones" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
 
-# AKS cluster definition
 resource "azurerm_kubernetes_cluster" "agones" {
   name                = var.cluster_name
   location            = azurerm_resource_group.agones.location
   resource_group_name = azurerm_resource_group.agones.name
-  dns_prefix          = "agones"
-  kubernetes_version  = var.kubernetes_version
+  # Don't change dns_prefix as node pool Network Security Group name uses a hash of dns_prefix on its name
+  dns_prefix = "agones"
+
+  kubernetes_version = var.kubernetes_version
 
   default_node_pool {
-    name            = "default"
-    node_count      = var.node_count
-    vm_size         = var.machine_type
-    os_disk_size_gb = var.disk_size
+    name                  = "default"
+    node_count            = var.node_count
+    vm_size               = var.machine_type
+    os_disk_size_gb       = var.disk_size
+    enable_auto_scaling   = false
+    enable_node_public_ip = var.enable_node_public_ip
   }
 
-  identity {
-    type = "SystemAssigned"
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
   }
 
   tags = {
@@ -84,7 +61,6 @@ resource "azurerm_kubernetes_cluster" "agones" {
   }
 }
 
-# Additional node pool for system workloads
 resource "azurerm_kubernetes_cluster_node_pool" "system" {
   name                  = "system"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.agones.id
@@ -92,13 +68,14 @@ resource "azurerm_kubernetes_cluster_node_pool" "system" {
   node_count            = 1
   os_disk_size_gb       = var.disk_size
   enable_auto_scaling   = false
-  node_taints           = ["agones.dev/agones-system=true:NoExecute"]
+  node_taints = [
+    "agones.dev/agones-system=true:NoExecute"
+  ]
   node_labels = {
-    "agones.dev/agones-system" = "true"
+    "agones.dev/agones-system" : "true"
   }
 }
 
-# Additional node pool for metrics workloads
 resource "azurerm_kubernetes_cluster_node_pool" "metrics" {
   name                  = "metrics"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.agones.id
@@ -106,13 +83,14 @@ resource "azurerm_kubernetes_cluster_node_pool" "metrics" {
   node_count            = 1
   os_disk_size_gb       = var.disk_size
   enable_auto_scaling   = false
-  node_taints           = ["agones.dev/agones-metrics=true:NoExecute"]
+  node_taints = [
+    "agones.dev/agones-metrics=true:NoExecute"
+  ]
   node_labels = {
-    "agones.dev/agones-metrics" = "true"
+    "agones.dev/agones-metrics" : "true"
   }
 }
 
-# Network security rule for game server
 resource "azurerm_network_security_rule" "gameserver" {
   name                       = "gameserver"
   priority                   = 100
@@ -123,12 +101,22 @@ resource "azurerm_network_security_rule" "gameserver" {
   destination_port_range     = "7000-8000"
   source_address_prefix      = "*"
   destination_address_prefix = "*"
-  resource_group_name        = azurerm_resource_group.agones.name
+  resource_group_name        = azurerm_kubernetes_cluster.agones.node_resource_group
+  # We don't use azurerm_resources datasource to get the security group as it's not reliable: random empty resource array
+  # 55978144 are the first 8 characters of the fnv64a hash's UInt32 of master node's dns prefix ("agones")
   network_security_group_name = "aks-agentpool-55978144-nsg"
 
   depends_on = [
     azurerm_kubernetes_cluster.agones,
-    azurerm_kubernetes_cluster_node_pool.system,
-    azurerm_kubernetes_cluster_node_pool.metrics
+    azurerm_kubernetes_cluster_node_pool.metrics,
+    azurerm_kubernetes_cluster_node_pool.system
   ]
+
+  # Ignore resource_group_name changes because of random case returned by AKS Api (MC_* or mc_*)
+  lifecycle {
+    ignore_changes = [
+      resource_group_name
+    ]
+  }
 }
+
