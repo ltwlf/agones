65b451ba68488f8c49f7b5c44041ce5854731a83
Christian Glessner <chris@leitwolf.io>
2024-12-03 19:18:49 +0100

    Update aks.tf

--- install/terraform/modules/aks/aks.tf
+++ install/terraform/modules/aks/aks.tf
@@ -1,10 +1,24 @@
-terraform {
-  required_version = ">= 1.3.0"
+# Copyright 2019 Google LLC All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+
+terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"  # Updated to the latest major version
+      version = "~> 4"
     }
   }
 }
@@ -13,82 +27,65 @@ provider "azurerm" {
   features {}
 }
 
-# Define Variables or use a variables.tf file
-variable "resource_group_location" {
-  description = "The Azure location where the resource group will be created."
-  type        = string
-  default     = "East US"
+resource "azurerm_resource_group" "agones" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-  default     = "agones-rg"
+resource "azurerm_kubernetes_cluster" "agones" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.agones.location
+  resource_group_name = azurerm_resource_group.agones.name
+  # don't change dns_prefix as node pool Network Security Group name uses a hash of dns_prefix on on its name
+  dns_prefix = "agones"
+
+  kubernetes_version = var.kubernetes_version
+
+  default_node_pool {
+    name                  = "default"
+    node_count            = var.node_count
+    vm_size               = var.machine_type
+    os_disk_size_gb       = var.disk_size
+    enable_auto_scaling   = false
+    enable_node_public_ip = var.enable_node_public_ip
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+  tags = {
+    Environment = "Production"
+  }
 }
 
-variable "cluster_name" {
-  description = "The name of the AKS cluster."
-  type        = string
-  default     = "agones-aks"
+resource "azurerm_kubernetes_cluster_node_pool" "system" {
+  name                  = "system"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.agones.id
+  vm_size               = var.machine_type
+  node_count            = 1
+  os_disk_size_gb       = var.disk_size
+  enable_auto_scaling   = false
+  node_taints = [
+    "agones.dev/agones-system=true:NoExecute"
+  ]
+  node_labels = {
+    "agones.dev/agones-system" : "true"
+  }
 }
 
-variable "kubernetes_version" {
-  description = "The Kubernetes version."
-  type        = string
-  default     = "1.25.4"
-}
-
-variable "node_count" {
-  description = "Number of nodes in the default node pool."
-  type        = number
-  default     = 3
-}
-
-variable "machine_type" {
-  description = "The VM size for the nodes."
-  type        = string
-  default     = "Standard_DS2_v2"
-}
-
-variable "disk_size" {
-  description = "OS disk size in GB."
-  type        = number
-  default     = 30
-}
-
-variable "enable_node_public_ip" {
-  description = "Whether to assign public IPs to nodes."
-  type        = bool
-  default     = false
-}
-
-variable "client_id" {
-  description = "Service Principal Client ID."
-  type        = string
-}
-
-variable "client_secret" {
-  description = "Service Principal Client Secret."
-  type        = string
-  sensitive   = true
-}
-
-module "aks_cluster" {
-  source = "git::https://github.com/ltwlf/agones.git?ref=main"  # Update the source as needed
-
-  resource_group_location = var.resource_group_location
-  resource_group_name     = var.resource_group_name
-  cluster_name            = var.cluster_name
-  kubernetes_version      = var.kubernetes_version
-  node_count              = var.node_count
-  machine_type            = var.machine_type
-  disk_size               = var.disk_size
-  enable_node_public_ip   = var.enable_node_public_ip
-  client_id               = var.client_id
-  client_secret           = var.client_secret
-
-  providers = {
-    azurerm = azurerm
+resource "azurerm_kubernetes_cluster_node_pool" "metrics" {
+  name                  = "metrics"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.agones.id
+  vm_size               = var.machine_type
+  node_count            = 1
+  os_disk_size_gb       = var.disk_size
+  enable_auto_scaling   = false
+  node_taints = [
+    "agones.dev/agones-metrics=true:NoExecute"
+  ]
+  node_labels = {
+    "agones.dev/agones-metrics" : "true"
   }
 }
 
@@ -102,16 +99,18 @@ resource "azurerm_network_security_rule" "gameserver" {
   destination_port_range     = "7000-8000"
   source_address_prefix      = "*"
   destination_address_prefix = "*"
-  resource_group_name        = module.aks_cluster.node_resource_group
-
-  # NSG Name based on hashed dns_prefix
+  resource_group_name        = azurerm_kubernetes_cluster.agones.node_resource_group
+  # We don't use azurerm_resources datasource to get the security group as it's not reliable: random empty resource array
+  # 55978144 are the first 8 characters of the fnv64a hash's UInt32 of master node's dns prefix ("agones")
   network_security_group_name = "aks-agentpool-55978144-nsg"
 
   depends_on = [
-    module.aks_cluster,
+    azurerm_kubernetes_cluster.agones,
+    azurerm_kubernetes_cluster_node_pool.metrics,
+    azurerm_kubernetes_cluster_node_pool.system
   ]
 
-  # Ignore changes to resource_group_name due to case sensitivity issues
+  # Ignore resource_group_name changes because of random case returned by AKS Api (MC_* or mc_*)
   lifecycle {
     ignore_changes = [
       resource_group_name
